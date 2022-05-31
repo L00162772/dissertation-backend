1970920e907fd91d2dae89e95bff3e044911d19a
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-31 21:51:37 +0100

    Update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "s3-website-policy" {
   }
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket" "lambda_bucket" {
   bucket        = "${var.aws_region}-${var.bucket_name_postfix}"
   force_destroy = true
   acl           = "private"
