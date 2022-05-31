de3c10a2de865e9f4cf496fae036dff6724ab596
Damien Gallagher <L00162772@student.lyit.ie>
2022-05-31 21:34:58 +0100

    Update

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -28,32 +28,32 @@ resource "aws_lb_listener" "alb_http" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      host        = local.cloudfront_domain
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.crud_lambda_tg.arn
   }
 }
 
-# resource "aws_lb_listener" "alb_https" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = aws_acm_certificate.alb_cert.arn
 
-#   default_action {
-#     type = "redirect"
+resource "aws_lb_target_group" "crud_lambda_tg" {
+  name        = "crud_lambda_tg"
+  target_type = "lambda"
 
-#     redirect {
-#       host        = local.cloudfront_domain
-#       status_code = "HTTP_301"
-#     }
-#   }
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
 
-#   depends_on = [
-#     aws_acm_certificate.alb_cert
-#   ]
-# }
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+    timeout  = 5
+    interval = 10
+  }
+}
+
+# See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group_attachment
+resource "aws_lb_target_group_attachment" "crud_lambda_tg" {
+  target_group_arn = aws_lb_target_group.crud_lambda_tg.arn
+  target_id        = aws_lambda_function.crud_lambda_function.arn
+  depends_on       = [aws_lambda_permission.with_lb_hello_world_1, aws_lb_target_group.crud_lambda_tg]
+}
+
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -16,31 +16,10 @@ data "aws_iam_policy_document" "s3-website-policy" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket        = "${var.aws_region}-${var.bucket_name_postfix}"
   force_destroy = true
-  acl           = "public-read"
+  acl           = "private"
 }
 
 
-resource "aws_s3_bucket_website_configuration" "s3_bucket_configuration" {
-  bucket = aws_s3_bucket.s3_bucket.bucket
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "s3_access_control" {
-  bucket             = aws_s3_bucket.s3_bucket.id
-  block_public_acls  = true
-  ignore_public_acls = true
-}
-
-resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  policy = data.aws_iam_policy_document.s3-website-policy.json
-}
 
 
