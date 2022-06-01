ceb209f07385c3fb3f81461396d06a3b1fb869e6
Damien Gallagher <L00162772@student.lyit.ie>
2022-06-01 22:29:24 +0100

    Update

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -33,6 +33,22 @@ resource "aws_lb_listener" "alb_http" {
   }
 }
 
+resource "aws_lb_listener" "alb_http_to_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener" "alb_https" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "443"
