f085711ca369629fb5a97f874c8f068ffa96708a
Damien Gallagher <L00162772@student.lyit.ie>
2022-06-03 22:43:42 +0100

    Update

--- scripts/setup_global_accelerator.py
+++ scripts/setup_global_accelerator.py
@@ -132,8 +132,8 @@ if len(list_listeners_response['Listeners']) <= 0:
         AcceleratorArn=accelerator_arn,
         PortRanges=[
             {
-                'FromPort': 80,
-                'ToPort': 80
+                'FromPort': 443,
+                'ToPort': 443
             },
         ],
         Protocol='TCP',
