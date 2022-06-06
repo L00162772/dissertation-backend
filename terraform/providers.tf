e5c024ce322957e06e400e721580921d0f387e39
Damien Gallagher <L00162772@student.lyit.ie>
2022-06-06 22:27:40 +0100

    Update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -22,6 +22,13 @@ terraform {
 data "aws_caller_identity" "current" {}
 provider "aws" {
   region = var.aws_region
+
+  skip_requesting_account_id  = true # this can be tricky
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+
   #profile = "lyit"
   default_tags {
     tags = {
