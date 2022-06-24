19e509d1a21ccae4818d9db91a2543066ef41bcc
Damien Gallagher <damien.gallagher@gmail.com>
2022-06-24 23:37:07 +0100

    Update

---
+++ terraform/backend/jest-dynamodb-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    tables: [
+      {
+        TableName: `users`,
+        KeySchema: [{AttributeName: 'id', KeyType: 'HASH'}],
+        AttributeDefinitions: [{AttributeName: 'id', AttributeType: 'S'}],
+        ProvisionedThroughput: {ReadCapacityUnits: 1, WriteCapacityUnits: 1},
+      },
+      // etc
+    ],
+  };
